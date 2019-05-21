f7a49e6bb1ab6da4cfcbe88f0164f72aa988e8f4
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-05-21 14:26:34 +0530

    queries and mutations

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+    renderSongs() {
+        if (this.props.data.loading) { return <div>Loading...</div>; }
+        return this.props.data.songs.map(song => {
+            return (
+                <li key={song.id} className="collection-item">
+                    {song.title}
+                </li>
+            );
+        });
+    }
     render() {
         return (
             <div>
-                SongList
+                <ul className="collection">
+                    {this.renderSongs()}
+                </ul>
+                <Link to="/songs/new" className="btn-floating btn-large red right"><i className="material-icons">add</i></Link>
             </div>
+
+
         );
     }
 }
 
-export default SongList;
+export default graphql(query)(SongList);
